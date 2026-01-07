8f577e7fe92bb2071aea83840c380038d6fe3724
Saiful Alam Rakib <msa4rakib@gmail.com>
2026-01-07 19:20:53 +0000

    Update environment variable access in constants for Vite compatibility

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,2 +1,2 @@
-export const apiKey = process.env.API_KEY || '';
-export const projectID = process.env.PROJECT_ID || '';
+export const apiKey = import.meta.env.VITE_API_KEY || "";
+export const projectID = import.meta.env.VITE_PROJECT_ID || "pockets";
