2a367d4bc1c0ad37e6093a1d885d2e7c609c0288
Saiful Alam Rakib <msa4rakib@gmail.com>
2026-01-19 20:50:38 +0600

    updated constant file

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -3,4 +3,4 @@ export const apiKey = process.env.NEXT_PUBLIC_API_KEY || "";
 export const projectID = process.env.NEXT_PUBLIC_PROJECT_ID || "pockets";
 export const commentsProjectID =
     process.env.NEXT_PUBLIC_PROJECT_DISCUSSION_ID || "pockets-discussion";
-export const ADMIN_PIN = process.env.NEXT_PUBLIC_ADMIN_PIN || "";
+export const ADMIN_PIN = process.env.ADMIN_PIN || "";
