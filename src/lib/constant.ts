fe77553148ff561ddc298b85a83f47205bcd3b04
Saiful Alam Rakib <msa4rakib@gmail.com>
2026-01-20 23:30:04 +0600

    fix: update BASE_API_URL to include versioning in the API endpoint

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,5 +1,5 @@
 export const BASE_API_URL =
-    process.env.BASE_API_URL || "https://silo.msar.dev/api";
+    process.env.BASE_API_URL || "https://silo.msar.dev/api/v1";
 export const apiKey = process.env.API_KEY || "";
 export const projectID = process.env.PROJECT_ID || "pockets";
 export const commentsProjectID =
