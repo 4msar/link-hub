e47cb28bceded460c1846f0adfed2eeb44f67d7a
Saiful Alam Rakib <msa4rakib@gmail.com>
2026-07-03 18:51:27 +0600

    fix: update BASE_API_URL to point to the new API endpoint

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,5 +1,5 @@
 export const BASE_API_URL =
-    process.env.BASE_API_URL || "https://silo.msar.dev/api/v1";
+    process.env.BASE_API_URL || "https://pockets-api.msar.me/api/v1";
 export const apiKey = process.env.API_KEY || "";
 export const projectID = process.env.PROJECT_ID || "pockets";
 export const ADMIN_PIN = process.env.ADMIN_PIN || "";
